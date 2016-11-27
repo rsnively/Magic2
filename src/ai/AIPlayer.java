94701a780577f94a380fa304d0a3b4a226ed061e
rsnively <rjsnively@gmail.com>
2016-11-26 20:49:47 -0800

    fix some ai garbage

--- src/ai/AIPlayer.java
+++ src/ai/AIPlayer.java
@@ -1,6 +1,7 @@
 package ai;
 
 import game.Card;
+import game.CardZone;
 import game.Game;
 import game.Land;
 import game.Mana;
@@ -11,35 +12,49 @@ import java.util.Iterator;
 
 public class AIPlayer {
     public static void MainPhase(Player p) {
-        Iterator<Card> i = p.GetHand().iterator();
-        while(i.hasNext()) {
-            Card c = i.next();
+        for (Iterator<Card> it = p.GetHand().iterator(); it.hasNext(); ) {
+            Card c = it.next();
             if (c.Is(Card.Type.Land) && c.CanPlay()) {
-                Game.Get().PlayCard(c);
+                it.remove();
+                c.Resolve();
             }
             else if (AIDecision.Decide() && CanPlay(p, c)) {
+                it.remove();
                 PayForCard(p, c);
-                Game.Get().PlayCard(c);
+                if (c.UsesStack()) {
+                    Game.Get().GetStack().AddEffect(c);
+                    c.SetZone(CardZone.Stack);
+                }
+                else
+                    c.Resolve();
             }
         }
     }
 
     public static boolean CanPlay(Player p, Card card) {
+        if (!card.CanPlay())
+            return false;
         Mana available = new Mana();
-        for (Card c : p.GetPermanents())
-            if (c.Is(Card.Type.Land))
+        for (Iterator<Card> it = p.GetPermanents().iterator(); it.hasNext(); ) {
+            Card c = it.next();
+            if (c.Is(Card.Type.Land) && !c.IsTapped())
                 available.Add(((Land) c).ColorProduced());
+        }
         return available.IsEnoughFor(card.GetCost());
     }
 
     public static void PayForCard(Player p, Card c) {
         ManaCost remaining = c.GetCost().GetManaCost();
 
-        for (Mana.Color color : Mana.Color.values())
-            if (remaining.Has(color))
-                for (Card card : p.GetPermanents())
-                    if (card.Is(Card.Type.Land) && !card.IsTapped() && ((Land)card).ColorProduced() == color)
+        for (Mana.Color color : Mana.Color.values()) {
+            if (remaining.Has(color)) {
+                for (Iterator<Card> it = p.GetPermanents().iterator(); it.hasNext(); ) {
+                    Card card = it.next();
+                    if (card.Is(Card.Type.Land) && !card.IsTapped() && ((Land) card).ColorProduced() == color)
                         card.Tap();
+                }
+            }
+        }
         for (int i = 0; i < p.GetPermanents().size() && remaining.GetGeneric() > 0; i++) {
             Card card = p.GetPermanents().get(i);
             if (card.Is(Card.Type.Land) && !card.IsTapped())
